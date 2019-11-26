2141208dd2dc98bd44daef1da1a7bfe5001bd004
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-11-26 22:09:07 +0200

    Commit made using teleportHQ

---
+++ components/component.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="component-component">
+    <div class="component-container">
+      <a href="#" target="_blank" class="component-link">Link</a>
+      <h1 class="component-heading1">Heading Level 1</h1>
+      <p class="component-textblock">Paragraph</p>
+    </div>
+    <img src="/playground_assets/Screenshot 2019-11-25 at 18.02.22.png" class="component-image" />
+    <img src="/playground_assets/Screenshot 2019-11-25 at 17.58.22.png" class="component-image1" />
+    <img src="/playground_assets/IMG_20190123_134309-PANO.jpg" class="component-image2" />
+    <img src="/playground_assets/IMG_20190123_135833.jpg" class="component-image3" />
+    <img src="/playground_assets/facebook covers-03.jpg" class="component-image4" />
+    <img src="/playground_assets/facebook covers-02.jpg" class="component-image5" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppComponent',
+  props: {},
+}
+</script>
+
+<style scoped>
+.component-component {
+  width: 900px;
+  height: 671px;
+  display: block;
+  overflow: auto;
+  font-size: 16px;
+  font-family: Open Sans;
+  background-color: #fff;
+}
+.component-container {
+  width: 325px;
+  height: 167px;
+  background-color: rgba(20, 20, 20, 0.2);
+}
+.component-link {
+  color: #000;
+  cursor: pointer;
+  text-decoration: underline;
+}
+.component-heading1 {
+  font-size: 2em;
+  margin-top: 0.67em;
+  margin-bottom: 0.67em;
+}
+.component-textblock {
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+.component-image {
+  width: 200px;
+}
+.component-image1 {
+  width: 200px;
+}
+.component-image2 {
+  width: 200px;
+}
+.component-image3 {
+  width: 200px;
+}
+.component-image4 {
+  width: 200px;
+}
+.component-image5 {
+  width: 200px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="index-page"><app-component></app-component></div>
+</template>
+
+<script>
+import AppComponent from '../components/component'
+
+export default {
+  name: 'Page',
+  components: {
+    AppComponent,
+  },
+  head: {
+    title: 'fresh',
+  },
+}
+</script>
+
+<style scoped>
+.index-page {
+  width: 100%;
+  display: block;
+  overflow: auto;
+  font-size: 16px;
+  min-height: 100vh;
+  font-family: Open Sans;
+  background-color: #fff;
+}
+</style>
